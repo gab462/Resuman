fa4ddeded1dfca27c4858695e73643105c34c9ca
Gustavo Araujo Borges <gustavoaraujob1+1@gmail.com>
2020-10-04 17:37:05 -0300

    Refactoring

--- src/clj/resuman/handlers.clj
+++ src/clj/resuman/handlers.clj
@@ -11,7 +11,7 @@
   (let [data (:body parameters)]
     (db/insert-user db/config data)
     {:status 201
-     :body "POST successful"}))
+     :body "POST successful"})) ;; FIXME return id on insert?
 
 (defn get-user-by-id
   [{:keys [parameters]}]
@@ -23,16 +23,23 @@
   [{:keys [parameters]}]
   (let [id (get-in parameters [:path :id])
         body (:body parameters)
-        data (assoc body :id id)]
-    (db/update-user-by-id db/config data)
-    {:status 200
-     :body (db/get-user-by-id db/config id)})) ;;FIXME returns exception
+        data (assoc body :id id)
+        updated-count (db/update-user-by-id db/config data)]
+    (if (= 1 updated-count)
+      {:status 200
+       :body {:updated true
+              :user (db/get-user-by-id db/config (:path parameters))}}
+      {:status 404
+       :body {:error "Unable to update user"}})))
 
 (defn delete-user-by-id
   [{:keys [parameters]}]
   (let [id (:path parameters)
-        before-deleted (db/get-user-by-id db/config id)]
-    (db/delete-user-by-id db/config id)
-    {:status 200
-     :body {:deleted true
-            :user before-deleted}}))
+        before-deleted (db/get-user-by-id db/config id)
+        deleted-count (db/delete-user-by-id db/config id)]
+    (if (= 1 deleted-count)
+      {:status 200
+       :body {:deleted true
+              :user before-deleted}}
+      {:status 404
+       :body {:error "Unable to delete user"}})))
