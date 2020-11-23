7aa572ab98baeb87b132104b2512f10e777a249d
Gustavo Araujo Borges <gustavoaraujob1+1@gmail.com>
2020-11-23 20:21:44 -0300

    Added project addition

--- src/cljs/resuman/components/project_list.cljs
+++ src/cljs/resuman/components/project_list.cljs
@@ -1,5 +1,5 @@
 (ns resuman.components.project-list
-  (:require [ajax.core :refer [GET]]
+  (:require [ajax.core :refer [GET POST]]
             [helix.core :refer [defnc <> $]]
             [helix.dom :as d]
             [helix.hooks :as hooks]
@@ -10,6 +10,10 @@
   (let [[new set-new] (hooks/use-state false)
         [state actions] (use-app-state)
         init-projects (:init-projects actions)
+        [nproject set-nproject] (hooks/use-state {:user 1
+                                                  :source "#"
+                                                  :thumbnail ""
+                                                  :info ""})
         projects (:projects state)]
     (hooks/use-effect
      :once
@@ -25,5 +29,38 @@
        (d/button {:class '[bg-blue-500 text-white text-xl font-bold py-2 px-4 rounded w-full]
                   :on-click #(set-new (not new))}
                  "+ Add Project")
-       ; ($ new-project)
-       ))))
+       (d/div {:class '[bg-white shadow p-4 mb-4]}
+              (d/div {:class '[flex]}
+                     (d/img {:src "https://camo.githubusercontent.com/5656aa6cc7a441294142817cf8fdeccb27ebe768/687474703a2f2f692e696d6775722e636f6d2f464958626737562e706e67"
+                             :class '["w-1/3" rounded-lg mr-4]})
+                     (d/form {:class '[flex-none "w-2/3"]}
+                             (d/input {:class '[shadow border border-gray-300 rounded w-full text-xl]
+                                       :type "text"
+                                       :id "title"
+                                       :placeholder "Title"
+                                       :on-change #(set-nproject
+                                                     (assoc nproject
+                                                            :title
+                                                            (.. %
+                                                                -target
+                                                                -value)))})
+                             (d/input {:class '[shadow border border-gray-300 rounded w-full text-sm]
+                                       :type "text"
+                                       :placeholder "Description"
+                                       :on-change #(set-nproject
+                                                     (assoc nproject
+                                                            :description
+                                                            (.. %
+                                                                -target
+                                                                -value)))})))
+              (d/div {:class '[flex flex-row-reverse]}
+                     (d/button {:class '[bg-blue-500 text-white font-bold py-2 px-4 mr-4 rounded]
+                                :on-click
+                                ; (js/console.log (select-keys nproject [:user :title :description]))
+                                #(POST "http://localhost:4000/api/projects"
+                                                {:params (select-keys nproject [:title :description :user :info :source :thumbnail])
+                                                 :format :json
+                                                 :handler (fn [response]
+                                                            (set-new (not new)))})
+                                }
+                               "Add")))))))
